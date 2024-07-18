7b821ed91064b0ea34847f9e838b108676d1540d
Пользователь <yaroslav-rusin@mail.ru>
2024-07-18 18:48:44 +0300

    add utils for getting weather forecast

--- weather/forecast/utils.py
+++ weather/forecast/utils.py
@@ -1,5 +1,5 @@
 import openmeteo_requests
-
+import pandas as pd
 import requests_cache
 from retry_requests import retry
 
@@ -9,10 +9,12 @@ openmeteo = openmeteo_requests.Client(session=retry_session)
 
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-    "latitude": 52.52,
-    "longitude": 13.41,
-    "hourly": "temperature_2m",
-    "timezone": "Europe/Moscow"
+    "latitude": 56.151382,
+    "longitude": 101.634152,
+    "hourly": ["temperature_2m"],
+    "timezone": "Europe/Moscow",
+    "start_date": "2024-07-19",
+    "end_date": "2024-07-19"
 }
 
 responses = openmeteo.weather_api(url, params=params)
@@ -22,3 +24,16 @@ print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
 print(f"Elevation {response.Elevation()} m asl")
 print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
 print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+    freq=pd.Timedelta(seconds=hourly.Interval()),
+    inclusive="left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+
+hourly_dataframe = pd.DataFrame(data=hourly_data)
+print(hourly_dataframe)
